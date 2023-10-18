1ee467a8d51dc00d422db21ba7e8095d41ff36ea
prishitameshram <54324852+prishitameshram@users.noreply.github.com>
2023-10-17 19:16:14 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            navLinks.forEach(l => l.classList.remove('active'));
+            link.classList.add('active');
+        });
+    });
+
+    const aboutButton = document.getElementById('aboutButton');
+
+    aboutButton.addEventListener('click', function() {
+        const aboutSection = document.getElementById('about');
+        aboutSection.style.display = 'block';
+    });
+
+    // Function to display the contact alert
+    function showContactAlert() {
+        alert('Please mail me at pmeshram@hawk.iit.edu');
+    }
+
+    const contactButton = document.getElementById('contactButton');
+    
+    // Add a click event listener to the "Contact Me" button
+    contactButton.addEventListener('click', showContactAlert);
+});
+
+document.getElementById("screen-width").innerHTML = 
+"Screen width is " + screen.width;
+
+ // Get the user's location and display it in the "Contact" section.
+ function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            const userLocation = document.getElementById("userLocation");
+            userLocation.textContent = `Latitude: ${position.coords.latitude}, Longitude: ${position.coords.longitude}`;
+        }, function(error) {
+            const userLocation = document.getElementById("userLocation");
+            userLocation.textContent = "Location information unavailable.";
+        });
+    } else {
+        const userLocation = document.getElementById("userLocation");
+        userLocation.textContent = "Geolocation is not supported by your browser.";
+    }
+}
+getLocation();
