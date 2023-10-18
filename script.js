241a6c726c0f485823ec1edd5c592cae539d48ca
prishitameshram <54324852+prishitameshram@users.noreply.github.com>
2023-10-17 21:38:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -15,29 +15,37 @@ document.addEventListener('DOMContentLoaded', function() {
         aboutSection.style.display = 'block';
     });
 
+    // Function to display the contact alert
+    function showAlert() {
+        alert('Please mail me at pmeshram@hawk.iit.edu');
+    }
+
+    const screenWidthElement = document.getElementById("screen-width-value");
+    if (screenWidthElement) {
+        screenWidthElement.textContent = "Screen width is " + screen.width;
+    }
+
+    // Get the user's location and display it in the "Contact" section.
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+                const userLocation = document.getElementById("userLocation");
+                if (userLocation) {
+                    userLocation.textContent = `Latitude: ${position.coords.latitude}, Longitude: ${position.coords.longitude}`;
+                }
+            }, function(error) {
+                const userLocation = document.getElementById("userLocation");
+                if (userLocation) {
+                    userLocation.textContent = "Location information unavailable.";
+                }
+            });
+        } else {
+            const userLocation = document.getElementById("userLocation");
+            if (userLocation) {
+                userLocation.textContent = "Geolocation is not supported by your browser.";
+            }
+        }
+    }
+
+    getLocation();
 });
-
-// Function to display the contact alert
-function showAlert() {
-    alert('Please mail me at pmeshram@hawk.iit.edu');
-    }
-    
-document.getElementById("screen-width").innerHTML = 
-"Screen width is " + screen.width;
-
- // Get the user's location and display it in the "Contact" section.
- function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            const userLocation = document.getElementById("userLocation");
-            userLocation.textContent = `Latitude: ${position.coords.latitude}, Longitude: ${position.coords.longitude}`;
-        }, function(error) {
-            const userLocation = document.getElementById("userLocation");
-            userLocation.textContent = "Location information unavailable.";
-        });
-    } else {
-        const userLocation = document.getElementById("userLocation");
-        userLocation.textContent = "Geolocation is not supported by your browser.";
-    }
-}
-getLocation();
